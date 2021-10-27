452e9f70bdda04865d12dca5f3ccd1834eea3d9f
LizaChelishev <lizachelishev@gmail.com>
2021-10-27 22:25:23 +0300

    done

--- homework_3.py
+++ homework_3.py
@@ -1,2 +1,3 @@
 list = [1, 2, 3, 4, 5, 7, 100, 89, 85]
 for i in range(0, (len(list)//2)):
+    print(list[i])
--- homework_5.py
+++ homework_5.py
@@ -1,6 +1,9 @@
 list = ['liza', 'is', 'studying', 'python']
 for word in list:
+    if len(word) < 4:
+        break
     print(word.upper())
+
 
 
 
